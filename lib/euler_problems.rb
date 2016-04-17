7eb7e88895a81ac787d89dbafe9d130f4603cbaa
Julie Torres <reeddetorres@gmail.com>
2016-04-16 22:02:13 -0400

    Update benchmarks

--- lib/euler_problems.rb
+++ lib/euler_problems.rb
@@ -5,16 +5,17 @@ files.each {|f| require f }
 
 # Euler 1
 array = Multiples.up_to(1000)
-euler1 = Multiples.sum(array)
-puts "Euler 1 answer is #{euler1}"
+puts "Euler 1 answer is #{ Multiples.sum(array) }"
+puts Benchmark.measure { Multiples.sum(array) }
 
 # Euler 2
 fibs = Fibonacci.up_to(4000000)
 evens = Fibonacci.evens(fibs)
-sum = Fibonacci.sum(evens)
-puts "Euler 2 answer is #{sum}"
+puts "Euler 2 answer is #{Fibonacci.sum(evens) }"
+puts Benchmark.measure { Fibonacci.sum(evens) }
 
 # Euler 3
-puts "Time to run: "
-puts Benchmark.measure { PrimeFactorizer.max_factor(600851475143)}
 puts "Euler 3 answer is #{PrimeFactorizer.max_factor(600851475143)}"
+puts Benchmark.measure { PrimeFactorizer.max_factor(600851475143)}
+
+# Euler 4
